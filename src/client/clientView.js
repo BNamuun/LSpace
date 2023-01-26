b51939dc0ef60b248527c90fa9c71957cc43c685
Evening <evening@admins-iMac.local>
2023-01-26 16:55:00 +0800

    Footer

--- src/client/clientView.js
+++ src/client/clientView.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { TableCIR } from "../Charts/tableCIR";
 import { TableCME } from "../Charts/tableCME";
 import { TableCMEFast } from "../Charts/tableCMEFast";
+import { Footer } from "../components/Footer";
 import NavTop from "../components/nav";
 import { HomePage } from "./HomePage";
 
@@ -14,6 +15,7 @@ export function ClientView() {
         <Route path="/cirList" element={<TableCIR />} />
         <Route path="/cmeList" element={<TableCMEFast />} />
       </Routes>
+      <Footer />
     </>
   );
 }
