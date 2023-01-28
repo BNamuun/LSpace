c69a59cda95e8d085f2440ad43d3a3b8a17ba642
Namuun <mit.irnitu@gmail.com>
2023-01-28 23:33:00 +0800

    title name change

--- src/Charts/Graph.js
+++ src/Charts/Graph.js
@@ -6,7 +6,7 @@ import { Sunspot } from "./SSN";
 export function Graph() {
   return (
     <div className="container px-2 py-5" id="custom-cards">
-      <h2 class="pb-2 border-bottom">Мэдээ мэдээлэл</h2>
+      <h2 class="pb-2 border-bottom">Мониторинг</h2>
       <div class="chartsPart d-flex mt-3 flex-wrap justify-content-around">
         <div class="ChartSize col-lg-5 col-md-6 col-sm-12 col-12 me-2 mb-5 ">
           <h4 class="text-black text-center"> Kp Геосоронзон индекс</h4>
